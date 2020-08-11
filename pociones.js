a79d37ce8fe9de2feb40cfa741f22f6c30b115c2
jhonleonquispe <quispej42@gmail.com>
2020-08-11 09:30:37 -0500

    Update pociones.js

--- pociones.js
+++ pociones.js
@@ -1,10 +1,11 @@
+let CantidadPociones;
 setTimeout(function(){ Pociones(); }, 15000);
 function Pociones(){
 	let claseDePocion = ".btn.potion-slot-button.blue.bigger";
 	let pociones = document.querySelector(claseDePocion);
-	let CantidadPociones = pociones.length;
-  console.log("Cantidad de pociones " + CantidadPociones);
-	if(pociones.length == 1 ){
+	if(pociones == undefined || pociones == null ) { CantidadPociones = 0;} else {  CantidadPociones = pociones.length }
+	console.log("Cantidad de pociones " + CantidadPociones);
+	if(CantidadPociones == 1 ){
 		pociones.click();
 	}
 	setTimeout(function(){console.log("intentos");location.reload;},600000);

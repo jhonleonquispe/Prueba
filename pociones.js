b66cfb738df5c5334a23b3a603507679d8c6bb1b
jhonleonquispe <quispej42@gmail.com>
2020-08-11 11:30:37 -0500

    changes

--- pociones.js
+++ pociones.js
@@ -1,4 +1,3 @@
-
 let CantidadPociones;
 setTimeout(function(){ Pociones(); }, 15000);
 function Pociones(){
@@ -6,7 +5,7 @@ function Pociones(){
 	let pociones = document.querySelectorAll(claseDePocion);
 	if(pociones == undefined || pociones == null ) { CantidadPociones = 0;} else {  CantidadPociones = pociones.length }
 	console.log("Cantidad de pociones " + CantidadPociones);
-	if(CantidadPociones > 1 ){
+	if(CantidadPociones >= 1 ){
 		pociones[0].click();
 	}
 	setTimeout(function(){console.log("intentos");location.reload();},600000);

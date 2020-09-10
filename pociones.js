78ba0c8944a0701965db67390e45925d40cc7531
jhonleonquispe <quispej42@gmail.com>
2020-09-10 10:35:56 -0500

    Update pociones.js

--- pociones.js
+++ pociones.js
@@ -1,7 +1,10 @@
 let CantidadPociones;
-setTimeout(function(){ Pociones(); }, 15000);
+let claseDePocion = ".btn.potion-slot-button.blue.bigger";
+let Clase_login = "#loginout"
+
+setTimeout(function(){ ValidarLogeoAndEjecute(); }, 15000);
+
 function Pociones(){
-	let claseDePocion = ".btn.potion-slot-button.blue.bigger";
 	let pociones = document.querySelectorAll(claseDePocion);
 	if(pociones == undefined || pociones == null ) { CantidadPociones = 0;} else {  CantidadPociones = pociones.length }
 	console.log("Cantidad de pociones " + CantidadPociones);
@@ -9,4 +12,18 @@ function Pociones(){
 		pociones[0].click();
 	}
 	setTimeout(function(){console.log("intentos");location.reload();},600000);
+}
+
+function ValidarLogeoAndEjecute(){
+	let loginout = document.querySelector(Clase_login);
+	if(loginout.innerText == "Login"){
+	loginout.click()
+	console.log("Intento login");
+	document.querySelector("#btnFacebookLoginAT").click();
+	setTimeout(function(){ Pociones(); }, 6000);
+	}
+	else {
+	 Pociones();
+	}
+	
 }
